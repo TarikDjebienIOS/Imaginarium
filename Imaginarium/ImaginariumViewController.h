259f97ba047f2edadee4744d600f7bf8fc62e251
Tarik Djebien <tarik@pc12047.osyris-sa-industrie.local>
2013-05-02 14:23:25 +0200

    Now under source control

---
+++ Imaginarium/ImaginariumViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImaginariumViewController.h
+//  Imaginarium
+//
+//  Created by Tarik Djebien on 02/05/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImaginariumViewController : UIViewController
+
+@end
